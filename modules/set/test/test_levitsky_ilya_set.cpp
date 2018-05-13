e3c5151247599dbf349e33aed8ec6758d5e26a75
Trintal <iliyalev@yandex.ru>
2018-05-13 20:49:37 +0300

    qwer

--- modules/set/test/test_levitsky_ilya_set.cpp
+++ modules/set/test/test_levitsky_ilya_set.cpp
@@ -1,9 +1,11 @@
 // Copyright 2018 Levitsky Ilya
 
 #include <gtest/gtest.h>
-#include <string>
-#include <vector>
 #include "include/set.h"
+
+class Levitsky_Ilya_setTest : public:resting::Test{
+ protected:
+};
 
 TEST(Levitsky_Ilya_setTest, Can_Create_Set) {
   // Arrange
@@ -148,29 +150,3 @@ TEST(Levitsky_Ilya_setTest, Can_Copy) {
     // Assert
     EXPECT_EQ(5, B.GetN(5));
 }
-class SetTest : public ::testing::Test {
- protected:
-    // virtual void SetUp() {}
-
-    void Act(vector<string> args_) {
-        vector<const char*> options;
-
-        options.push_back("appname");
-        for (size_t i = 0; i < args_.size(); ++i) {
-            options.push_back(args_[i].c_str());
-        }
-
-        const char** argv = &options.front();
-        int argc = static_cast<int>(args_.size()) + 1;
-
-        output_ = app_(argc, argv);
-    }
-
-    void Assert(std::string expected) {
-        EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
-    }
-
- private:
-    Set A;
-    string output_;
-};
