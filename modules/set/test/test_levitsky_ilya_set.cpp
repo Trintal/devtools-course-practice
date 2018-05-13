2af4a5955b9dfa5401ceb47fef27487f2f263bd1
Trintal <iliyalev@yandex.ru>
2018-05-13 19:44:42 +0300

    class settest

--- modules/set/test/test_levitsky_ilya_set.cpp
+++ modules/set/test/test_levitsky_ilya_set.cpp
@@ -147,3 +147,29 @@ TEST(Levitsky_Ilya_setTest, Can_Copy) {
     // Assert
     EXPECT_EQ(5, B.GetN(5));
 }
+class SetTest : public ::testing::Test {
+protected:
+	// virtual void SetUp() {}
+
+	void Act(vector<string> args_) {
+		vector<const char*> options;
+
+		options.push_back("appname");
+		for (size_t i = 0; i < args_.size(); ++i) {
+			options.push_back(args_[i].c_str());
+		}
+
+		const char** argv = &options.front();
+		int argc = static_cast<int>(args_.size()) + 1;
+
+		output_ = app_(argc, argv);
+	}
+
+	void Assert(std::string expected) {
+		EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+	}
+
+private:
+	Set A;
+	string output_;
+};
