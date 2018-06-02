240db4553d96ca73aa248c9aea56a391b4c6743d
Trintal <iliyalev@yandex.ru>
2018-06-02 18:22:35 +0300

    back

--- modules/area_of_geometric_shapes/src/GeometryT.cpp
+++ modules/area_of_geometric_shapes/src/GeometryT.cpp
@@ -100,7 +100,7 @@ std::string GeometryT::operator()(int argc, const char** argv) {
             args.height = parseDouble(argv[2]);
             break;
         case 's':
-            args.height = parseDouble(argv[2]);
+            args.radius = parseDouble(argv[2]);
             break;
         default:
             args.radius = parseDouble(argv[2]);
